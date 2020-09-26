0112f627df8f30b4d19ebffe8b773152503982b6
7798209972 <patlesachin1@gmail.com>
2020-09-26 16:05:44 +0530

    Flipping Coin to Display Head or Tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -6,3 +6,12 @@ echo "Welcome to Flip Coin Combination Problem"
 echo "========================================"
 printf "\n";
 
+#Flipping Coin to display Head or Tail
+
+flip1=$(( RANDOM %2+1 ));
+if [ $flip1 -eq 1 ]
+then
+	echo "Head";
+else
+	echo "Tail";
+fi
